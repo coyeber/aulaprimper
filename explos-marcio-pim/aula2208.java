1c7da2046a24ebb500966f5379fe6aec144d3ad8
coyeber <sergiobjunior232@gmail.com>
2025-08-23 00:34:26 +0000

    hello

---
+++ explos-marcio-pim/aula2208.java
@@ -0,0 +1,28 @@
+public class aula2208 {
+    public static void main(String[] args) {
+       
+        for ( int i = 1; i <=100; i++ ){
+        System.out.print(i+",");
+
+        }
+
+        for ( int i = 3; i <=99; i++ ){
+        System.out.print(i+",");
+
+    }
+    for ( int i = 3; i <=99; i++ ){
+        if ( i % 2 == 0){
+        System.out.print(i+",");
+        }
+
+}
+ for ( int i = 3; i <=100; i++ ){
+        if ( i % 3 == 0){
+        System.out.print(i+",");
+
+        }
+
+    }
+
+    } 
+}
