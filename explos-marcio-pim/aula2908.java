62078e3cf8cbe7c8e49f9e07adc2e72e7ea8f5e2
coyeber <sergiobjunior232@gmail.com>
2025-08-30 00:38:49 +0000

    tentativa

---
+++ explos-marcio-pim/aula2908.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class aula2908 {
+    public static final int QTD = 4;
+    public static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+    for ( int i = 0; i < numeros.length; 1 += 1){
+        System.out.printlf("Digite %d numero:" , 1 + 1) ;
+        numeros[i] = input.nextInt();
+
+    }
+}
