570270579a494be3a08552f3da381ab3f5d3fcd1
coyeber <sergiobjunior232@gmail.com>
2025-09-13 00:25:38 +0000

    dia2124

---
+++ explos-marcio-pim/marcioexercvetores.java
@@ -0,0 +1,17 @@
+public class marcioexercvetores{
+    public static void main (String[] args) {
+        int[] vetor = new int[6];
+        vetor[0] = 1;
+        vetor[1] = 2;
+        int soma = A[0] + A[1] + A[5];
+        System.out.println(" A Soma dos trés valores é "+ soma);
+        A[4] = 100;
+        System.out.println("Impressão do vetor 1 em cada linha");
+        for (int i = 0; i < v.lengt; i += 1) {
+            System.out.println(A[i]);
+
+            int[] v = new int[6];
+                for (int i = 0; )
+        }
+    }
+}
---
+++ explos-marcio-pim/vetoresematrizes1.java
@@ -0,0 +1,7 @@
+public class vetoresmatrizes1 {
+    public static void main(String[] args) {
+
+        int[] numeros = {1 , 0, 5, -2, -5, 7};
+        System.out.println( numeros );
+    }
+}
